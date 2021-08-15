ba7e6fa4d686c8e85905376d919dcee81c96bcd4
Liberto Camús <liberto@lcamus.es>
2021-08-15 08:59:43 +0200

    Remove explicit declaration of Repository

--- backend/src/cats/cats.module.ts
+++ backend/src/cats/cats.module.ts
@@ -3,10 +3,9 @@ import { CatsService } from './cats.service';
 import { CatsResolver } from './cats.resolver';
 import { Cat } from './entities/cat.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Cat])],
-  providers: [CatsResolver, CatsService, Repository], // TODO: why to declare Repository?
+  providers: [CatsResolver, CatsService], 
 })
 export class CatsModule {}
