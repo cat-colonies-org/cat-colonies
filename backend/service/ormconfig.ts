59bc9870fb49485760bd3408cb4b4fadedf0cf39
David Díaz <daviddq@gmail.com>
2021-08-26 15:19:12 +0200

    Fix ORM config to dynamically pick the configuration

--- backend/service/ormconfig.ts
+++ backend/service/ormconfig.ts
@@ -2,13 +2,13 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: 'database',
-  port: 5432,
-  username: 'appuser',
-  password: 'appuser-pass!',
-  database: 'cats',
+  host: process.env.DB_HOST || 'database',
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || 'appuser',
+  password: process.env.DB_PASS || 'appuser-pass!',
+  database: process.env.DB_NAME || 'cats',
   entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
 };
 
 export default config;
