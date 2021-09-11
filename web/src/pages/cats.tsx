8f995261591b2145e125dedb40713b2cbc676a4a
David Díaz <daviddq@gmail.com>
2021-09-11 11:04:53 +0200

    Implement pagination (wip)

--- web/src/pages/cats.tsx
+++ web/src/pages/cats.tsx
@@ -1,16 +1,62 @@
+import DataTable from 'react-data-table-component';
+
+const totalRows: number = 100;
+let loading: boolean = false;
+
 interface Cat {
   id: number;
   sterilized: boolean;
   birthYear: number;
-  colony: {
-    address: string;
-    locationType: { description: string };
-    environment: { description: string };
-  };
+  colonyAddress: string;
+  colonyLocationType: string;
+  colonyEnvironment: string;
 }
 
+const handlePerRowsChange = () => {};
+const handlePageChange = () => {};
+
+const columns = [
+  { name: 'Id', selector: (cat: Cat) => cat.id },
+  { name: 'Nacimiento', selector: (cat: Cat) => cat.birthYear },
+  {
+    name: 'Esterilizado',
+    selector: (cat: Cat) => cat.sterilized,
+    format: (cat: Cat) => (cat.sterilized ? 'Sí' : 'No'),
+  },
+  { name: 'Dirección', selector: (cat: Cat) => cat.colonyAddress },
+  { name: 'Tipo de localización', selector: (cat: Cat) => cat.colonyLocationType },
+  { name: 'Entorno', selector: (cat: Cat) => cat.colonyEnvironment },
+];
+
+const Home = ({ cats }: { cats: Cat[] }) => {
+  return (
+    <>
+      <h1>Gatos</h1>
+
+      <p>Lista de gatos</p>
+
+      <DataTable
+        highlightOnHover={true}
+        striped={true}
+        columns={columns}
+        data={cats}
+        progressPending={loading}
+        pagination
+        paginationRowsPerPageOptions={[2, 4, 8, 16]}
+        // paginationServer
+        // paginationTotalRows={totalRows}
+        // onChangeRowsPerPage={handlePerRowsChange}
+        // onChangePage={handlePageChange}
+        onRowClicked={(row) => {
+          alert(JSON.stringify(row, null, 2));
+        }}
+      />
+    </>
+  );
+};
+
 const buildCatsQuery = () => `query {
-  cats (order: "id", descending: true, skip: 0, take: 4) {
+  cats (order: "id", descending: false) {
     id
     sterilized
     birthYear
@@ -22,42 +68,9 @@ const buildCatsQuery = () => `query {
   }
 }`;
 
-const Home = ({ cats }: { cats: Cat[] }) => {
-  return (
-    <>
-      <h1>Gatos</h1>
-
-      <p>Lista de gatos</p>
-
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Año</th>
-            <th scope="col">Esterilizado</th>
-            <th scope="col">Direción colonia</th>
-            <th scope="col">Tipo colonia</th>
-            <th scope="col">Entorno colonia</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cats.map((cat, idx) => (
-            <tr key={idx}>
-              <td>{cat.id}</td>
-              <td>{cat.birthYear}</td>
-              <td>{cat.sterilized}</td>
-              <td>{cat.colony.address}</td>
-              <td>{cat.colony.locationType.description}</td>
-              <td>{cat.colony.environment.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
-};
-
 export async function getServerSideProps() {
+  loading = true;
+
   const options = {
     method: 'post',
     headers: {
@@ -71,8 +84,18 @@ export async function getServerSideProps() {
   return fetch('http://service:8080/graphql', options)
     .then((response) => response.json())
     .then((response) => {
-      const cats = response.data?.cats;
-      console.log(JSON.stringify(cats));
+      const cats = response.data?.cats.map((cat: any): Cat => {
+        loading = false;
+
+        return {
+          id: cat.id,
+          sterilized: cat.sterilized,
+          birthYear: cat.birthYear,
+          colonyAddress: cat.colony.address,
+          colonyLocationType: cat.colony.locationType.description,
+          colonyEnvironment: cat.colony.environment.description,
+        };
+      });
       return { props: { cats } };
     });
 }
