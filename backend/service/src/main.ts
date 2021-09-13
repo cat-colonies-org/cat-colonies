1b90c72b9985188c399b0041af63180b94529787
David Díaz <daviddq@gmail.com>
2021-09-13 23:42:39 +0200

    Enable CORS

--- backend/service/src/main.ts
+++ backend/service/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 (async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('port');
 
