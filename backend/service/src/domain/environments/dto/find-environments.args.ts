dc7f9577f1840bf8e60a3277d3eb2766d3a9a345
David Díaz <daviddq@gmail.com>
2021-09-13 22:41:49 +0200

    Use BaseResolver for "Environments"

--- backend/service/src/domain/environments/dto/find-environments.args.ts
+++ backend/service/src/domain/environments/dto/find-environments.args.ts
@@ -2,7 +2,7 @@ import { ArgsType, Field, Int } from '@nestjs/graphql';
 import { FindArgs } from 'src/common/find.args';
 
 @ArgsType()
-export class FindEnvironmentArgs extends FindArgs {
+export class FindEnvironmentsArgs extends FindArgs {
   @Field(() => Int, { nullable: true })
   id?: number;
 
