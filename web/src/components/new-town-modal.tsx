a9484bd37d05661613401aa091b768095ea2b8e2
David Díaz <daviddq@gmail.com>
2021-09-23 17:00:24 +0200

    Refactor input modal

--- web/src/components/new-town-modal.tsx
+++ web/src/components/new-town-modal.tsx
@@ -1,8 +1,7 @@
-import { FormEvent, useRef, useState } from 'react';
-import Modal from 'react-modal';
 import { createTown } from '../services/towns';
+import InputModal, { InputModalResult } from './input-modal';
 
-interface NewModalProps {
+interface NewTownModalProps {
   id: string;
   isOpen: boolean;
   onRequestClose: any;
@@ -10,91 +9,30 @@ interface NewModalProps {
   onNewTownError: any;
 }
 
-const NewTownModal = ({ id, isOpen, onRequestClose, onNewTownSuccess, onNewTownError }: NewModalProps) => {
-  const [state, setState] = useState({ name: '' });
-  const nameInput = useRef<HTMLInputElement>(null);
+const NewTownModal = ({ id, isOpen, onRequestClose, onNewTownSuccess, onNewTownError }: NewTownModalProps) => {
+  const onCreateTown = async (result: InputModalResult): Promise<void> => {
+    if (result.value) {
+      const townName = result.value;
 
-  const onAfterOpen = () => {
-    setState({ name: '' });
-    nameInput?.current?.focus();
-  };
-
-  const onSubmit = (event: FormEvent): void => {
-    event.preventDefault();
-  };
-
-  const onInputChange = (event: FormEvent<HTMLInputElement>): void => {
-    setState({ ...state, [event.currentTarget.id]: event.currentTarget.value });
-  };
-
-  const onCreateTownClick = async (event: FormEvent<HTMLButtonElement>): Promise<void> => {
-    event.preventDefault();
-
-    if (state.name) {
-      const town = await createTown(state.name);
+      const town = await createTown(townName);
       if (town) {
         onNewTownSuccess(town);
       } else {
-        onNewTownError(state.name);
+        onNewTownError(townName);
       }
     }
-
-    onRequestClose();
   };
 
   return (
-    <Modal
+    <InputModal
       id={id}
+      title="Nueva Localidad"
+      caption="Nombre"
+      buttonCaption="Crear"
+      onClose={onRequestClose}
+      onReturn={onCreateTown}
       isOpen={isOpen}
-      onAfterOpen={onAfterOpen}
-      onRequestClose={onRequestClose}
-      shouldCloseOnOverlayClick={true}
-      shouldCloseOnEsc={true}
-      ariaHideApp={false}
-      shouldFocusAfterRender={true}
-      shouldReturnFocusAfterClose={true}
-      style={{
-        overlay: {
-          backgroundColor: 'rgba(0,0,0, 0.5)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        content: {
-          position: 'relative',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
-          padding: '40px',
-        },
-      }}
-    >
-      <form onSubmit={onSubmit}>
-        <div className="row">
-          <div className="col">
-            <h5>Nueva localidad</h5>
-            <hr />
-            <label htmlFor="id" className="form-label">
-              Nombre
-            </label>
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              value={state.name}
-              onChange={onInputChange}
-              placeholder="Nombre de la localidad"
-              ref={nameInput}
-            />
-            <button className="btn btn-primary mt-3" onClick={onCreateTownClick}>
-              Crear
-            </button>
-            <button className="btn btn-danger mt-3" onClick={onRequestClose}>
-              Cancelar
-            </button>
-          </div>
-        </div>
-      </form>
-    </Modal>
+    />
   );
 };
 
