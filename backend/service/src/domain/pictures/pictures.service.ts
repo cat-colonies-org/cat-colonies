c51716fb7fa6d44730451d8550ef8aef38dc87c3
David Díaz <daviddq@gmail.com>
2021-10-28 11:43:59 +0200

    Pictures service doesn't need the context to be injected

--- backend/service/src/domain/pictures/pictures.service.ts
+++ backend/service/src/domain/pictures/pictures.service.ts
@@ -1,13 +1,10 @@
 import { BaseCrudService } from 'src/common/base-crud.service';
-import { CONTEXT } from '@nestjs/graphql';
-import { Inject, Injectable, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Picture } from './entities/picture.entity';
 import { Repository } from 'typeorm';
 
-@Injectable({ scope: Scope.REQUEST })
 export class PicturesService extends BaseCrudService<Picture> {
-  constructor(@InjectRepository(Picture) repository: Repository<Picture>, @Inject(CONTEXT) private context) {
+  constructor(@InjectRepository(Picture) repository: Repository<Picture>) {
     super(repository);
   }
 }
