d931c0c5f144f274d9557aa231a66247b2a1cde0
David Díaz <daviddq@gmail.com>
2021-10-28 11:44:28 +0200

    Move private method inside its class

--- backend/service/src/file-upload/file-upload.controller.ts
+++ backend/service/src/file-upload/file-upload.controller.ts
@@ -1,29 +1,30 @@
+import { AuthGuard } from '@nestjs/passport';
 import { CatsService } from 'src/domain/cats/cats.service';
-import { Controller, Post, UseInterceptors, UploadedFiles, Param } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFiles, Param, UseGuards, Req } from '@nestjs/common';
 import { FileMetadata } from './dto/file-upload.input';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 import * as Path from 'path';
 
-const hasValidExtension = (file: FileMetadata): boolean => {
-  const ext = Path.extname(file.originalname).toLowerCase();
-  return ['.png', '.jpg', '.jpeg', '.webp'].includes(ext);
-};
-
-// TODO: autorización
 @Controller()
 export class FileUploadController {
   constructor(private readonly cats: CatsService, private readonly uploader: FileUploadService) {}
 
   @Post('/file-upload/:catId')
+  @UseGuards(AuthGuard('jwt'))
   @UseInterceptors(FilesInterceptor('pictures'))
   async uploadPicture(@Param('catId') catId: number, @UploadedFiles() files: FileMetadata[]) {
     const cat = await this.cats.findOne(catId);
     if (!cat) return { uploaded: 0 };
 
-    const validFiles = files.filter((file: FileMetadata) => hasValidExtension(file));
+    const validFiles = files.filter((file: FileMetadata) => this.hasValidExtension(file));
     for (const file of validFiles) await this.uploader.uploadPicture(catId, file);
 
     return { uploaded: validFiles.length };
   }
+
+  private hasValidExtension = (file: FileMetadata): boolean => {
+    const ext = Path.extname(file.originalname).toLowerCase();
+    return ['.png', '.jpg', '.jpeg', '.webp'].includes(ext);
+  };
 }
