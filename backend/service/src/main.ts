bc7974ac34e4a98810a3cc6f16b4f27e8c0ecdce
David Díaz <daviddq@gmail.com>
2021-10-28 15:02:41 +0200

    Minor fix defining httpOptions

--- backend/service/src/main.ts
+++ backend/service/src/main.ts
@@ -3,25 +3,27 @@ import { NestFactory } from '@nestjs/core';
 import * as fs from 'fs';
 import SettingsService from './settings/settings.service';
 
-const crPath = '/etc/letsencrypt/live/colonias.cincohocicos.com/fullchain.pem';
-const pkPath = '/etc/letsencrypt/live/colonias.cincohocicos.com/privkey.pem';
-const sslOptions: any = {};
+const CR_PATH = '/etc/letsencrypt/live/colonias.cincohocicos.com/fullchain.pem';
+const PK_PATH = '/etc/letsencrypt/live/colonias.cincohocicos.com/privkey.pem';
 
-if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
-  sslOptions.httpsOptions = {
-    cert: fs.readFileSync(crPath),
-    key: fs.readFileSync(pkPath),
-  };
+const httpsOptions: any = {};
+
+if (fs.existsSync(CR_PATH) && fs.existsSync(PK_PATH)) {
+  httpsOptions.cert = fs.readFileSync(CR_PATH);
+  httpsOptions.key = fs.readFileSync(PK_PATH);
 }
 
 (async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { ...sslOptions, cors: true });
+  const app = await NestFactory.create(AppModule, {
+    ...(httpsOptions && { httpsOptions }),
+    cors: true,
+  });
   const settings: SettingsService = app.get(SettingsService);
   const port: number = settings.port;
 
   await app.listen(port);
 
   console.log(`Service listening on port ${port}`);
-  console.log(`SSL certificate: ${sslOptions.cert ? 'available' : 'unavailable'}`);
-  console.log(`SSL key: ${sslOptions.key ? 'available' : 'unavailable'}`);
+  console.log(`SSL certificate: ${httpsOptions.cert ? 'available' : 'unavailable'}`);
+  console.log(`SSL key: ${httpsOptions.key ? 'available' : 'unavailable'}`);
 })();
