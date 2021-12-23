39e99b5200222b4848515b00d40e1897c85aa844
David Díaz <daviddq@gmail.com>
2021-12-23 16:11:24 +0100

    Build arm64 docker images for "web", "service" and "database"

--- backend/service/src/auth/auth.service.ts
+++ backend/service/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Repository } from 'typeorm';
 import { AccessToken } from './dto/access-token';
 import { JwtPayload } from './dto/jwt-payload.interface';
--- backend/service/src/domain/users/users.service.ts
+++ backend/service/src/domain/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { BaseCrudService } from 'src/common/base-crud.service';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
